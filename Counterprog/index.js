11d13e91ae514733653506657096b0e0ca3ff577
cxiara22 <cxiara22@gmail.com>
2024-05-30 11:18:35 +0800

    A simple counter to kickstart my jounrey in CSS, html and js
    Making the counter, understanding how function in JS work and linking between the 3 languages --> spin on this would be to include the bootstrap fw into the mix

--- Counterprog/index.js
+++ Counterprog/index.js
@@ -1,1 +1,27 @@
 
+const Decbtn = document.getElementById("dec-btn"); 
+const Resbtn = document.getElementById("res-btn"); 
+const Incbtn = document.getElementById("inc-btn"); 
+const countlabel = document.getElementById("countlabel"); 
+
+let count = 0; 
+
+function changelabel (counted){ 
+    countlabel.innerText = counted; 
+} 
+
+// embedding one function within another 
+Incbtn.onclick = function () {
+    changelabel(count++); 
+}
+
+// using arrow functions for even more concise, one-liner functions 
+Decbtn.onclick = () => changelabel((count--)); 
+
+// there is a need to run a function onclick, because if we were to just use the '=' sign, 
+// what happens is that you are passing in an undefined value since the function changelabel does not give anyth
+Resbtn.onclick = function(){
+    count = 0; 
+    changelabel (count); 
+}
+
